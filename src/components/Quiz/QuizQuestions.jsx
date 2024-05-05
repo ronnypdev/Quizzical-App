25e05445a5c754dc98b0a206c7844be7e8d8caa4
Ronny Perez <ronnypdev@gmail.com>
2024-05-05 18:09:40 -0400

    fix api connection and css

--- src/components/Quiz/QuizQuestions.jsx
+++ src/components/Quiz/QuizQuestions.jsx
@@ -1,41 +1,38 @@
 import { useState, useEffect } from 'react';
+import {decode} from 'html-entities';
 import Button from '../Base/Button';
 
 export default function QuizQuestions() {
+  const [quizData, setQuizData] = useState({});
 
-  const [quizData, setQuizData] = useState({})
+  const url = "https://opentdb.com/api.php?amount=5&category=31&difficulty=medium&type=multiple";
 
-  const url = "https://opentdb.com/api.php?amount=5&category=31&difficulty=medium&type=multiple"
-  
   // Fetch the data from the API
   useEffect(() => {
-
     fetch(url)
       .then(response => response.json())
       .then(data => setQuizData(data))
-      .catch((error) => {
-        console.warn("Something when wrong, please check: ", error);
-      })
-
   },[])
 
   function handleSubmit(event) {
-    event.preventDefault()
-    // submitToApi(formData)
+    event.preventDefault();
   }
+
+  // console.log("responseCode : ", quizData.response_code);
+  // console.log("results : ", quizData.results);
 
   return (
     <>
       <div className="Quizzical__Quiz">
         <form className="Quizzical__Form" onSubmit={handleSubmit}>
-          {quizData.results.map((quiz) =>
-            <div className="Quizzical__Form-Section" key={quizData.index}>
-              <p className="Quizzical__Question">{quiz.question}</p>
+          {quizData.results?.map((quiz) =>
+            <div className="Quizzical__Form-Section" key={quiz.correct_answer}>
+              <p className="Quizzical__Question">{decode(quiz.question)}</p>
               <div className="Quizzical__Answers-Wrapper">
-                {quiz.incorrect_answers.map((answers) =>
-                  <div className="Quizzical__Answers" key={answers.index}>
-                    <input className="input-radio" value="adios" type="radio" name={quiz.question} id="Question_1" />
-                    <label htmlFor="Question_1">{answers}</label>
+                {quiz.incorrect_answers?.map((answers, index) =>
+                  <div className="Quizzical__Answers" key={index}>
+                    <input className="input-radio" value={quiz.correct_answer} type="radio" name={quiz.difficulty} id={quiz.question}  />
+                    <label htmlFor={quiz.question}>{answers}</label>
                   </div>
               )}
               </div>
