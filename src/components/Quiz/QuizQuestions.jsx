1dc5db9491d23b2b667591b1eef283aca23d267d
Ronny Perez <ronnypdev@gmail.com>
2024-06-16 18:36:20 -0400

    Select all features pending

--- src/components/Quiz/QuizQuestions.jsx
+++ src/components/Quiz/QuizQuestions.jsx
@@ -1,10 +1,11 @@
 import { useState, useEffect } from 'react';
-import {decode} from 'html-entities';
+import { decode } from 'html-entities';
+import { nanoid } from 'nanoid'
 import Button from '../Base/Button';
 
 export default function QuizQuestions() {
   const [quizData, setQuizData] = useState([]);
-  let apiResults = null;
+  // let apiResults = null;
 
   const url = "https://opentdb.com/api.php?amount=5&category=31&difficulty=medium&type=multiple";
 
@@ -22,25 +23,21 @@ export default function QuizQuestions() {
     fetch(url)
       .then(response => response.json())
       .then(data => {
-        apiResults = data.results
+       const apiResults = data.results
         apiResults.map((question) => {
           question.allAnswers = [...question.incorrect_answers, question.correct_answer]
           question.allAnswers = shuffle(question.allAnswers)
+          question.sectionId = nanoid()
         })
-        console.log(apiResults)
+        console.log("apiResults : ", apiResults)
         setQuizData(apiResults)
       })
   }, [])
 
-  // this will handle the answer selection
-  function handleSelectAnswer() {
+  function getCorrectAnswer() {
 
   }
 
-  //function submit quiz data
-  function submitQuizData(quizInfo) {
-    
-  }
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -52,7 +49,7 @@ export default function QuizQuestions() {
       <div className="Quizzical__Quiz">
         <form className="Quizzical__Form" onSubmit={handleSubmit}>
           {quizData?.map((quiz) =>
-            <div className="Quizzical__Form-Section" key={quiz.correct_answer}>
+            <div className="Quizzical__Form-Section" key={quiz.sectionId}>
               <p className="Quizzical__Question">{decode(quiz.question)}</p>
               <div className="Quizzical__Answers-Wrapper">
                 {quiz.allAnswers?.map((answers, index) =>
@@ -63,7 +60,6 @@ export default function QuizQuestions() {
                       type="radio"
                       name={quiz.correct_answer}
                       value={quiz.correct_answer}
-                      onChange={handleSelectAnswer}
                     />
                     <label htmlFor={answers}>{decode(answers)}</label>
                   </div>
